1e3b3e34d465b65cec12190c786a8cf676a90906
DennisGambini <dennis.gambini@gmail.com>
2022-04-26 12:46:59 +0200

    index e style: prima impaginazione aside

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
 const app = new Vue({
     el:'#app',
     data:{
-
+        chats:[
+            {
+                nomeUtente: 'Michele',
+                immagineUtente: 'img/avatar_1.jpg',
+            },
+            {
+                nomeUtente: 'Fabio',
+                immagineUtente: 'img/avatar_2.jpg',
+            },
+            {
+                nomeUtente: 'Samuele',
+                immagineUtente: 'img/avatar_3.jpg',
+            },
+        ]
     },
     methods:{
-        
+
     }
 })
